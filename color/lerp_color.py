8033b3b78b344af08d8a3b486549c4b8b6084163
Abhik Pal <theabhikpal@gmail.com>
2017-07-22 22:12:23 +0530

    Add lerp_color example.

---
+++ color/lerp_color.py
@@ -0,0 +1,50 @@
+#
+# Lerp color
+#
+# Usage:
+# - Press <S> to randomize the start color.
+# - Press <E> to randomize the end color.
+# - Press <UP> to increase the number of colored bands
+# - Press <DOWN> to decrease the number of colored bands
+#
+
+from p5 import *
+
+start_color = Color(255)
+end_color = Color(0)
+lerp_bands = 5
+
+def setup():
+    size(640, 360)
+    no_stroke()
+    background(0)
+
+def draw():
+    background(0)
+    for bi in range(lerp_bands):
+        lerp_amount = bi/(lerp_bands - 1)
+        band_color = start_color.lerp(end_color, lerp_amount)
+        band_location = remap(bi, (0, lerp_bands), (0, width))
+        fill(band_color)
+        rect((band_location, 0), width/lerp_bands, height)
+
+def key_pressed(event):
+    global lerp_bands
+
+    if event.key == 's':
+        start_color.r = random_uniform(0, 255)
+        start_color.g = random_uniform(0, 255)
+        start_color.b = random_uniform(0, 255)
+
+    elif event.key == 'e':
+        end_color.r = random_uniform(0, 255)
+        end_color.g = random_uniform(0, 255)
+        end_color.b = random_uniform(0, 255)
+
+    elif event.key == 'UP':
+        lerp_bands = constrain(lerp_bands + 1, 3, 100)
+    elif event.key == 'DOWN':
+        lerp_bands = constrain(lerp_bands - 1, 3, 100)
+
+if __name__ == '__main__':
+    run()
