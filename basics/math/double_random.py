cc6e93c8087b56d3ce4287fae24a9b14dac457d4
Abhik Pal <theabhikpal@gmail.com>
2017-08-02 01:43:58 +0530

    Add double_random example.

---
+++ basics/math/double_random.py
@@ -0,0 +1,34 @@
+#
+# Double Random
+# by Ira Greenberg
+#
+# Python port: Abhik Pal
+#
+# Using two random() calls and the point() function to create an
+# irregular sawtooth line.
+
+from p5 import *
+
+total_points = 300
+steps = total_points + 1
+
+def setup():
+    size(640, 360)
+    title("Double Random")
+    background(0)
+    stroke(255)
+    set_frame_rate(1)
+
+def draw():
+    background(0)
+
+    rand = 0
+    for i in range(1, steps):
+        x = (width / steps) * i
+        y = (height / 2)  + random_uniform(-rand, rand)
+        point(x, y)
+
+        rand += random_uniform(-5, 5)
+
+if __name__ == '__main__':
+    run()
