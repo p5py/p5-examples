877fc3f82d4ac979153e3e850ffe8e571ff602af
Abhik Pal <theabhikpal@gmail.com>
2017-08-02 23:32:21 +0530

    Update list example.

--- basics/lists/list.py
+++ basics/lists/list.py
@@ -1,18 +1,29 @@
+# List
+#
+# A list is a data type in Python that can be used to group together
+# other values. Lists might contain values of different types but
+# usually the items have the same type. Each piece of data in a list
+# is identified by an index number representing its position in the
+# list. Lists are zero based, which means that the first element in
+# the array is [0], the second element is [1], and so on. In this
+# example, a list named "coswav" is created and filled with the cosine
+# values. This data is displayed three separate ways on the screen.
+
 from p5 import *
 
 coswave = []
 
 def setup():
     size(640, 360)
+    title("List")
 
-    for i in range(width):
+    for i in range(width + 1):
         amount = remap(i, (0, width), (0, PI))
         coswave.append(abs(cos(amount)))
 
-    background(255)
+    no_loop()
 
 def draw():
-    background(255)
     for i, cos_value in enumerate(coswave):
         stroke(cos_value * 255)
         line((i, 0), (i, height / 3))
@@ -22,5 +33,6 @@ def draw():
 
         stroke(255 - cos_value * 255)
         line((i, 2 * height / 3), (i, height))
-    
-run()
+
+if __name__ == '__main__':
+    run()
